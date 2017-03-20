f770baeb8348a3a31f73c19814ab7f84a69e12f3
seriushwa <seriushwa.cs00@gmail.com>
2017-03-20 13:53:25 +0800

    Change description
    change test name

--- triangle.cpp
+++ triangle.cpp
@@ -25,7 +25,7 @@ int triangle(int a,int b,int c)
 	return 2;//Scalene
 }
 
-TEST(Triangle,BoundarySR)
+TEST(Triangle,BoundaryRW)//robust worst-case
 {
 EXPECT_EQ(4,triangle(0	,0	,0	));
 EXPECT_EQ(4,triangle(0	,0	,1	));
@@ -378,25 +378,25 @@ EXPECT_EQ(4,triangle(201	,201	,200	));
 EXPECT_EQ(4,triangle(201	,201	,201	));
 }
 
-TEST(Triangle,EquivalenceSR)
+TEST(Triangle,Equivalence)
 {
-EXPECT_EQ(0,triangle(5,5,5));
-EXPECT_EQ(1,triangle(2,2,3));
-EXPECT_EQ(2,triangle(3,4,5));
-EXPECT_EQ(3,triangle(4,2,2));
-EXPECT_EQ(4,triangle(-1,5,5));
-EXPECT_EQ(4,triangle(5,-1,5));
-EXPECT_EQ(4,triangle(5,5,-1));
-EXPECT_EQ(4,triangle(201,5,5));
-EXPECT_EQ(4,triangle(5,201,5));
-EXPECT_EQ(4,triangle(5,5,201));
-EXPECT_EQ(4,triangle(-1,5,5));
-EXPECT_EQ(4,triangle(5,-1,5));
-EXPECT_EQ(4,triangle(5,5,-1));
-EXPECT_EQ(4,triangle(-1,-1,5));
-EXPECT_EQ(4,triangle(5,-1,-1));
-EXPECT_EQ(4,triangle(-1,5,-1));
-EXPECT_EQ(4,triangle(-1,-1,-1));
+EXPECT_EQ(0,triangle(5,5,5));//SN
+EXPECT_EQ(1,triangle(2,2,3));//SN
+EXPECT_EQ(2,triangle(3,4,5));//SN
+EXPECT_EQ(3,triangle(4,2,2));//SN
+EXPECT_EQ(4,triangle(-1,5,5));//WR
+EXPECT_EQ(4,triangle(5,-1,5));//WR
+EXPECT_EQ(4,triangle(5,5,-1));//WR
+EXPECT_EQ(4,triangle(201,5,5));//WR
+EXPECT_EQ(4,triangle(5,201,5));//WR
+EXPECT_EQ(4,triangle(5,5,201));//WR
+EXPECT_EQ(4,triangle(-1,5,5));//SR
+EXPECT_EQ(4,triangle(5,-1,5));//SR
+EXPECT_EQ(4,triangle(5,5,-1));//SR
+EXPECT_EQ(4,triangle(-1,-1,5));//SR
+EXPECT_EQ(4,triangle(5,-1,-1));//SR
+EXPECT_EQ(4,triangle(-1,5,-1));//SR
+EXPECT_EQ(4,triangle(-1,-1,-1));//SR
 }
 
 TEST(Triangle,DecisionTable)
