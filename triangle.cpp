78ef137cb859c974caebd3b206cd82e3bca1fc0a
seriushwa <seriushwa.cs00@gmail.com>
2017-04-29 22:57:42 +0800

    Modify feature code for drawing program graph

--- triangle.cpp
+++ triangle.cpp
@@ -2,25 +2,34 @@
 #include"gtest/gtest.h"
 int triangle(int a,int b,int c)
 {
-	if((a<1)||(b<1)||(c<1))return 4;//input too small
-	if((a>200)||(b>200)||(c>200))return 4;//input too large
-	if((a+b<=c)||(a+c<=b)||(b+c<=a))return 3;//cannot form a triangle
+	if((a<1)||(b<1)||(c<1))
+		return 4;//input too small
+	if((a>200)||(b>200)||(c>200))
+		return 4;//input too large
+	if((a+b<=c)||(a+c<=b)||(b+c<=a))
+		return 3;//cannot form a triangle
 	if(a==b)
 	{
-		if(a!=c)return 1;//Isosceles
-		else return 0;//Equilateral
+		if(a!=c)
+			return 1;//Isosceles
+		else 
+			return 0;//Equilateral
 	}
 	if(a==c)
 	{
 	
-		if(a!=b)return 1;//Isosceles
-		else return 0;//Equilateral
+		if(a!=b)
+			return 1;//Isosceles
+		else 
+			return 0;//Equilateral
 	}
 	if(b==c)
 	{
 	
-		if(b!=a)return 1;//Isosceles
-		else return 0;//Equilateral
+		if(b!=a)
+			return 1;//Isosceles
+		else 
+			return 0;//Equilateral
 	}
 	return 2;//Scalene
 }
